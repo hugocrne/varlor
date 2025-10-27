faa054d687ce860929fc5d05102aa96a995c6482
hugocrne <hugo.creneau@etu.uca.fr>
2025-10-27 14:52:25 +0100

    push

---
+++ gateway/src/test/kotlin/com/vardor/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vardor.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
